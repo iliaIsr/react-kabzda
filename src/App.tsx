af802af24ee148fd3438bde78873fa24ed113456
iliaIsr <ilil.sbl91@gmail.com>
2024-05-25 10:50:22 +0300

    kabzda lesson 6.2

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,33 @@ import React from 'react';
 import './App.css';
 import {Acordion} from "./components/accordion/Accordion";
 import {Rating} from "./components/rating/Rating";
+import {OnOf} from "./components/onOf/OnOf";
+import {UncontrollAcordion} from "./components/accordion/UncontrollAccordion";
+import {UncontrollRating} from "./components/rating/UncontrollRating";
 
 
 function App() {
     return (
-        <div>
-            <PageTitle title={"This is App compoent"}/>
-            <PageTitle title={"My friends"}/>
-            Article 1
-            <Rating value={3}/>
-            <Acordion titleValue={"Menu"} collapset={false}/>
-            <Acordion titleValue={"Users"} collapset={true}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
+        <div className={"App"}>
+            {/*<PageTitle title={"This is App compoent"}/>*/}
+            {/*<PageTitle title={"My friends"}/>*/}
+            {/*Article 1*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Acordion titleValue={"Menu"} collapset={false}/>*/}
+            {/*<Acordion titleValue={"Users"} collapset={true}/>*/}
+            {/*<Rating value={0}/>*/}
+            {/*<Rating value={1}/>*/}
+            {/*<Rating value={2}/>*/}
+            {/*<Rating value={3}/>*/}
+            {/*<Rating value={4}/>*/}
+            {/*<Rating value={5}/>*/}
+            <UncontrollAcordion titleValue={"Menu"} />
+            <UncontrollAcordion titleValue={"Users"} />
+            <UncontrollRating/>
+            <OnOf/>
+            <OnOf/>
+            <OnOf/>
+
         </div>
     );
 }
