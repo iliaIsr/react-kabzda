30d52d6230081e5a7fd3642a0ac6a382b5b81a7d
iliaIsr <ilil.sbl91@gmail.com>
2024-06-10 10:39:34 +0300

    kabzda lesson 9

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Acordion} from "./components/accordion/Accordion";
-import {Rating} from "./components/rating/Rating";
+import {Rating, RatingValueType} from "./components/rating/Rating";
 import {OnOf} from "./components/onOf/OnOf";
+import {OnOfUncontroll} from "./components/onOf/OnOfUncontroll";
 import {UncontrollAcordion} from "./components/accordion/UncontrollAccordion";
 import {UncontrollRating} from "./components/rating/UncontrollRating";
 
 
 function App() {
+    let [ratingValue, setRetingValue]=useState<RatingValueType>(0)
+    let [accordionCollapset, setAccordionCollapset]=useState<boolean>(false)
+    let[switchOn,setswitchOn]=useState(false)
     return (
         <div className={"App"}>
             {/*<PageTitle title={"This is App compoent"}/>*/}
             {/*<PageTitle title={"My friends"}/>*/}
             {/*Article 1*/}
             {/*<Rating value={3}/>*/}
-            {/*<Acordion titleValue={"Menu"} collapset={false}/>*/}
+            {/*<Acordion titleValue={"Menu"} collapset={accordionCollapset} onClick={setAccordionCollapset}/> */}
+            <Acordion titleValue={"Menu"} collapset={accordionCollapset} onClick={()=>{setAccordionCollapset(!accordionCollapset)}}/>
             {/*<Acordion titleValue={"Users"} collapset={true}/>*/}
             {/*<Rating value={0}/>*/}
             {/*<Rating value={1}/>*/}
@@ -22,12 +27,13 @@ function App() {
             {/*<Rating value={3}/>*/}
             {/*<Rating value={4}/>*/}
             {/*<Rating value={5}/>*/}
-            <UncontrollAcordion titleValue={"Menu"} />
+            {/*<UncontrollAcordion titleValue={"Menu"} />*/}
             <UncontrollAcordion titleValue={"Users"} />
-            <UncontrollRating/>
-            <OnOf/>
-            <OnOf/>
-            <OnOf/>
+            <Rating value={ratingValue} onClick={setRetingValue}/>
+            {/*<UncontrollRating/>*/}
+            {/*<OnOf onClick={setswitchOn} on={switchOn}/>*/}
+            {/*<OnOf/>*/}
+            <OnOfUncontroll onChange={setswitchOn}/>{switchOn.toString()}
 
         </div>
     );
--- src/components/onOf/OnOf.tsx
+++ src/components/onOf/OnOf.tsx
@@ -1,13 +1,14 @@
-import React, {useState} from 'react';
-
+import React, {MouseEventHandler, useState} from 'react';
+type onType=boolean
 type PropsTypeOnOf={
-    //on:boolean
+    on:onType
+    onClick:(on:onType)=>void
 }
 
 
 export const OnOf = (props:PropsTypeOnOf) => {
 
-    let[on,setOn]=useState(false)
+
 
 
     const onStyle={
@@ -16,7 +17,7 @@ export const OnOf = (props:PropsTypeOnOf) => {
         border: "1px solid black",
         display:"inline-block",
         padding:'2px',
-        backgroundColor:on?"green":"white"
+        backgroundColor:props.on?"green":"white"
     }
     const offStyle={
         width:"30px",
@@ -25,7 +26,7 @@ export const OnOf = (props:PropsTypeOnOf) => {
         display:"inline-block",
         marginLeft: "2px",
         padding:'2px',
-        backgroundColor: on?"white":"red"
+        backgroundColor: props.on?"white":"red"
     }
     const indicatorStyle={
         width:"10px",
@@ -34,12 +35,13 @@ export const OnOf = (props:PropsTypeOnOf) => {
         border:"1px solid black",
         display:"inline-block",
         marginLeft:"5px",
-        backgroundColor: on?"green":"red"
+        backgroundColor: props.on?"green":"red"
     }
+
     return (
         <div>
-            <div style={onStyle} onClick={()=>{setOn(true)}}>on</div>
-            <div style={offStyle} onClick={()=>{setOn(false)}}>off</div>
+            <div style={onStyle} onClick={()=>props.onClick(true)}>on</div>
+            <div style={offStyle} onClick={()=>props.onClick(false)}>off</div>
             <div style={indicatorStyle}></div>
         </div>
     );
